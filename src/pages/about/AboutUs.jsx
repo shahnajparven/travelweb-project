d96f34789dd0563162c4d18437a5d208488ad22a
shahnaj parven <shahnajritu21@gmail.com>
2023-11-18 00:06:25 +0600

    change

--- src/pages/about/AboutUs.jsx
+++ src/pages/about/AboutUs.jsx
@@ -15,7 +15,6 @@ const AboutUs = () => {
           <Box width={400}>
             <img src={img1} alt="" height={250} width={"100%"} />
           </Box>
-          
         </Box>
         <Box width={"50%"}>
           <Box display={"flex"} flexDirection={"column"}>
@@ -45,7 +44,6 @@ const AboutUs = () => {
               adipisicing elit. Deleniti praesentium, numquam magni quae fugit
               necessitatibus eos sit doloribus distinctio quaerat.{" "}
               <Box py={1}>
-                {" "}
                 Maiores magnam quo officiis numquam consectetur doloremque
                 deleniti temporibus assumenda?
               </Box>
@@ -55,23 +53,28 @@ const AboutUs = () => {
       </Box>
       <Box>
         <Box display={"flex"} justifyContent={"center"} gap={10} pt={10}>
-          <Box display={"flex"} gap={1} alignItems={'center'}>
-            <Box className='font' fontWeight={'bold'} fontSize={30}>120+</Box>
+          <Box display={"flex"} gap={1} alignItems={"center"}>
+            <Box className="font" fontWeight={"bold"} fontSize={30}>
+              120+
+            </Box>
             <Box>Total Destination</Box>
           </Box>
-          <Box display={"flex"} gap={1} alignItems={'center'}>
-            {" "}
-            <Box className='font' fontWeight={'bold'} fontSize={30}>500+</Box>
+          <Box display={"flex"} gap={1} alignItems={"center"}>
+            <Box className="font" fontWeight={"bold"} fontSize={30}>
+              500+
+            </Box>
             <Box>Travel Packages</Box>
           </Box>
-          <Box display={"flex"} gap={1} alignItems={'center'}>
-            {" "}
-            <Box className='font' fontWeight={'bold'} fontSize={30}>12k+</Box>
+          <Box display={"flex"} gap={1} alignItems={"center"}>
+            <Box className="font" fontWeight={"bold"} fontSize={30}>
+              12k+
+            </Box>
             <Box>Total Travelers</Box>
           </Box>
-          <Box display={"flex"} gap={1} alignItems={'center'}>
-            {" "}
-            <Box className='font' fontWeight={'bold'} fontSize={30}>7k+</Box>
+          <Box display={"flex"} gap={1} alignItems={"center"}>
+            <Box className="font" fontWeight={"bold"} fontSize={30}>
+              7k+
+            </Box>
             <Box>Positive Reviews</Box>
           </Box>
         </Box>
